46f4588fc2c89e7d55ac51adb1177d4dc4788c1a
Alexis Wurth <alexis.wurth57@gmail.com>
2016-12-24 12:54:43 +0100

    Added controllers

---
+++ src/App/Controller/CategoryController.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\Category;
+use App\Model\Property;
+use Respect\Validation\Validator as V;
+use Psr\Http\Message\RequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+class CategoryController extends Controller
+{
+    public function add(Request $request, Response $response)
+    {
+        if ($request->isPost()) {
+            $this->validator->validate($request, ['name' => V::notBlank()]);
+
+            $parent = null;
+            if ($request->getParam('parent_id')) {
+                $parent = Category::find($request->getParam('parent_id'));
+
+                if (!$parent) {
+                    $this->validator->addError('parent_id', 'Parent category does not exist');
+                }
+            }
+
+            $propertiesIds = $request->getParam('properties');
+            $properties = $propertiesIds ? Property::whereIn('id', $propertiesIds)->get() : null;
+
+            if ($propertiesIds && count($propertiesIds) != $properties->count()) {
+                $this->validator->addError('properties', 'One or more properties don\'t exist');
+            }
+
+            if ($this->validator->isValid()) {
+                $category = new Category(['name' => $request->getParam('name')]);
+
+                if ($parent) {
+                    $category->parent()->associate($parent);
+                }
+
+                $category->save();
+
+                if ($properties) {
+                    $category->properties()->attach($properties);
+                }
+
+                $this->flash('success', 'Category "' . $category->name . '" added');
+                return $this->redirect($response, 'category.get');
+            }
+        }
+
+        return $this->view->render($response, 'Category/add.twig', [
+            'categories' => Category::whereDoesntHave('parent')->get(),
+            'properties' => Property::all()
+        ]);
+    }
+
+    public function edit(Request $request, Response $response, $id)
+    {
+        $category = Category::with('properties')->find($id);
+
+        if (!$category) {
+            throw $this->notFoundException($request, $response);
+        }
+
+        if ($request->isPost()) {
+            $this->validator->validate($request, ['name' => V::notBlank()]);
+
+            $parent = null;
+            if ($request->getParam('parent_id')) {
+                $parent = Category::find($request->getParam('parent_id'));
+
+                if (!$parent) {
+                    $this->validator->addError('parent_id', 'Parent category does not exist');
+                }
+            }
+
+            $propertiesIds = $request->getParam('properties');
+            $properties = $propertiesIds ? Property::whereIn('id', $propertiesIds)->get() : null;
+
+            if ($propertiesIds && count($propertiesIds) != $properties->count()) {
+                $this->validator->addError('properties', 'One or more properties don\'t exist');
+            }
+
+            if ($this->validator->isValid()) {
+                $category->name = $request->getParam('name');
+
+                if ($parent) {
+                    $category->parent()->associate($parent);
+                } else {
+                    $category->parent()->dissociate();
+                }
+
+                $category->save();
+
+                $category->properties()->detach();
+                if ($properties) {
+                    $category->properties()->attach($properties);
+                }
+
+                $this->flash('success', 'Category "' . $category->name . '" edited');
+                return $this->redirect($response, 'category.get');
+            }
+        }
+
+        return $this->view->render($response, 'Category/edit.twig', [
+            'categories' => Category::whereDoesntHave('parent')->get(),
+            'category' => $category,
+            'properties' => Property::all()
+        ]);
+    }
+
+    public function delete(Request $request, Response $response, $id)
+    {
+        $category = Category::find($id);
+
+        if (!$category) {
+            throw $this->notFoundException($request, $response);
+        }
+
+        foreach ($category->subCategories as $subCategory) {
+            $subCategory->parent()->dissociate();
+            $subCategory->save();
+        }
+
+        $category->equipments()->detach();
+        $category->properties()->detach();
+
+        $category->delete();
+
+        $this->flash('success', 'Category "' . $category->name . '" deleted');
+        return $this->redirect($response, 'category.get');
+    }
+
+    public function get(Request $request, Response $response)
+    {
+        $categories = Category::with('subCategories')->where('parent_id', null)->get();
+
+        return $this->view->render($response, 'Category/get.twig', [
+            'categories' => $categories
+        ]);
+    }
+}
---
+++ src/App/Controller/ItemController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controller;
+
+use Psr\Http\Message\RequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+class ItemController extends Controller
+{
+
+}
