6313513ed4c8ff5a30e309598a1a2220e9289d15
Alexis Wurth <alexis.wurth57@gmail.com>
2016-12-27 14:26:19 +0100

    Removed useless route

--- src/App/Resources/routes/product.php
+++ src/App/Resources/routes/product.php
@@ -3,5 +3,4 @@
 $app->map(['GET', 'POST'], '/products/add', 'ProductController:add')->setName('product.add');
 $app->map(['GET', 'POST'], '/products/{id:[0-9]+}/edit', 'ProductController:edit')->setName('product.edit');
 $app->get('/products/{id:[0-9]+}/delete', 'ProductController:delete')->setName('product.delete');
-$app->get('/products/{id:[0-9]+}', 'ProductController:show')->setName('product.show');
 $app->get('/products', 'ProductController:get')->setName('product.get');
