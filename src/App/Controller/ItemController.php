df00c62bc29df6169e7d6171b29946c42e3c08c6
Alexis Wurth <alexis.wurth57@gmail.com>
2017-02-16 18:31:43 +0100

    Item resource is now RESTful

--- src/App/Controller/ItemController.php
+++ src/App/Controller/ItemController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Model\Item;
 use App\Model\Product;
-use App\Model\Property;
 use Respect\Validation\Validator as V;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
@@ -12,65 +11,79 @@ use Psr\Http\Message\ResponseInterface as Response;
 class ItemController extends Controller
 {
     /**
+     * Get items list
+     *
+     * @param Request $request
+     * @param Response $response
+     * @return Response
+     */
+    public function getCollection(Request $request, Response $response)
+    {
+        return $this->ok($response, Item::with('product')->get());
+    }
+
+    /**
+     * Get one item
+     *
+     * @param Request $request
+     * @param Response $response
+     * @param string $id
+     * @return Response
+     */
+    public function get(Request $request, Response $response, $id)
+    {
+        $item = Item::find($id);
+
+        if (null === $item) {
+            throw $this->notFoundException($request, $response);
+        }
+
+        return $this->ok($response, $item);
+    }
+
+    /**
      * Add item
      *
      * @param Request $request
      * @param Response $response
      * @return Response
      */
-    public function add(Request $request, Response $response)
+    public function post(Request $request, Response $response)
     {
-        if ($request->isPost()) {
-            $this->validator->validate($request, [
-                'code' => V::notBlank()->alnum(),
-                'purchased_at' => V::notBlank()->date('d/m/Y'),
-                'repaired_at' => V::optional(V::date('d/m/Y')),
-                'product_id' => V::notBlank()
-            ], [
-                'notBlank' => 'Ce champ est requis',
-                'alnum' => 'Le code ne peut contenir que des lettres et des chiffres',
-                'date' => '{{name}} n\'est pas une date valide'
-            ]);
+        $this->validator->validate($request, [
+            'code' => V::notBlank()->alnum(),
+            'purchased_at' => V::notBlank()->date('d/m/Y'),
+            'product_id' => V::notBlank()
+        ], [
+            'notBlank' => 'Ce champ est requis',
+            'alnum' => 'Le code ne peut contenir que des lettres et des chiffres',
+            'date' => '{{name}} n\'est pas une date valide'
+        ]);
 
-            if (Item::where('code', $request->getParam('code'))->first()) {
-                $this->validator->addError('code', 'Ce code existe déjà');
-            }
-
-            $product = Product::find($request->getParam('product_id'));
-
-            if (!$product) {
-                $this->validator->addError('product_id', 'Produit inconnu');
-            }
-
-            $propertiesData = $request->getParam('property');
-            $properties = $propertiesData ? Property::whereIn('id', array_keys($propertiesData))->get() : null;
-
-            if ($propertiesData && count($propertiesData) != $properties->count()) {
-                $this->validator->addError('properties', 'Une ou plusieurs propriétés n\'existent pas');
-            }
-
-            if ($this->validator->isValid()) {
-                $repairedAt = $request->getParam('repaired_at');
-                $item = new Item([
-                    'code' => $request->getParam('code'),
-                    'purchased_at' => \DateTime::createFromFormat('d/m/Y', $request->getParam('purchased_at')),
-                    'repaired_at' => $repairedAt ? \DateTime::createFromFormat('d/m/Y', $repairedAt) : null
-                ]);
-                $item->product()->associate($product);
-                $item->save();
-
-                foreach ($propertiesData as $id => $value) {
-                    $item->properties()->attach($id, ['value' => $value]);
-                }
-
-                $this->flash('success', 'Article "' . $item->name . '" ajouté');
-                return $this->redirect($response, 'item.get');
-            }
+        if (Item::where('code', $request->getParam('code'))->first()) {
+            $this->validator->addError('code', 'Ce code existe déjà');
         }
 
-        return $this->view->render($response, 'Item/add.twig', [
-            'products' => Product::all()
-        ]);
+        $product = Product::find($request->getParam('product_id'));
+
+        if (null === $product) {
+            $this->validator->addError('product_id', 'Produit inconnu');
+        }
+
+        if ($this->validator->isValid()) {
+            $item = new Item([
+                'code' => $request->getParam('code'),
+                'purchased_at' => \DateTime::createFromFormat('d/m/Y', $request->getParam('purchased_at'))
+            ]);
+            $item->product()->associate($product);
+            $item->save();
+
+            return $this->created($response, 'get_item', [
+                'id' => $item->id
+            ]);
+        }
+
+        return $this->validationErrors($response);
     }
 
     /**
@@ -81,61 +94,37 @@ class ItemController extends Controller
      * @param string $id
      * @return Response
      */
-    public function edit(Request $request, Response $response, $id)
+    public function put(Request $request, Response $response, $id)
     {
         $item = Item::find($id);
 
-        if (!$item) {
+        if (null === $item) {
             throw $this->notFoundException($request, $response);
         }
 
-        if ($request->isPost()) {
-            $this->validator->validate($request, [
-                'purchased_at' => V::notBlank()->date('d/m/Y'),
-                'repaired_at' => V::optional(V::date('d/m/Y')),
-                'product_id' => V::notBlank()
-            ], [
-                'notBlank' => 'Ce champ est requis',
-                'date' => '{{name}} n\'est pas une date valide'
-            ]);
+        $this->validator->validate($request, [
+            'purchased_at' => V::notBlank()->date('d/m/Y'),
+            'product_id' => V::notBlank()
+        ], [
+            'notBlank' => 'Ce champ est requis',
+            'date' => '{{name}} n\'est pas une date valide'
+        ]);
 
-            $product = Product::find($request->getParam('product_id'));
+        $product = Product::find($request->getParam('product_id'));
 
-            if (!$product) {
-                $this->validator->addError('product_id', 'Produit inconnu');
-            }
-
-            $propertiesData = $request->getParam('property');
-            $properties = $propertiesData ? Property::whereIn('id', array_keys($propertiesData))->get() : null;
-
-            if ($propertiesData && count($propertiesData) != $properties->count()) {
-                $this->validator->addError('properties', 'Une ou plusieurs propriétés n\'existent pas');
-            }
-
-            if ($this->validator->isValid()) {
-                $item->purchased_at = \DateTime::createFromFormat('d/m/Y', $request->getParam('purchased_at'));
-                $repairedAt = $request->getParam('repaired_at');
-                if ($repairedAt) {
-                    $item->repaired_at = \DateTime::createFromFormat('d/m/Y', $repairedAt);
-                }
-                $item->product()->associate($product);
-                $item->save();
-
-                $newProperties = [];
-                foreach ($propertiesData as $propId => $value) {
-                    $newProperties[$propId] = ['value' => $value];
-                }
-                $item->properties()->sync($newProperties);
-
-                $this->flash('success', 'Article "' . $item->code . '" modifié');
-                return $this->redirect($response, 'item.get');
-            }
+        if (null === $product) {
+            $this->validator->addError('product_id', 'Produit inconnu');
         }
 
-        return $this->view->render($response, 'Item/edit.twig', [
-            'item' => $item,
-            'products' => Product::all()
-        ]);
+        if ($this->validator->isValid()) {
+            $item->purchased_at = \DateTime::createFromFormat('d/m/Y', $request->getParam('purchased_at'));
+            $item->product()->associate($product);
+            $item->save();
+
+            return $this->noContent($response);
+        }
+
+        return $this->validationErrors($response);
     }
 
     /**
@@ -150,28 +139,12 @@ class ItemController extends Controller
     {
         $item = Item::find($id);
 
-        if (!$item) {
+        if (null === $item) {
             throw $this->notFoundException($request, $response);
         }
 
-        $item->properties()->detach();
         $item->delete();
 
-        $this->flash('success', 'Article "' . $item->code . '" supprimé');
-        return $this->redirect($response, 'item.get');
-    }
-
-    /**
-     * Get items list
-     *
-     * @param Request $request
-     * @param Response $response
-     * @return Response
-     */
-    public function get(Request $request, Response $response)
-    {
-        return $this->view->render($response, 'Item/get.twig', [
-            'items' => Item::with('product')->get()
-        ]);
+        return $this->noContent($response);
     }
 }
