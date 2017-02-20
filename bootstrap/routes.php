ebf76da3f64d93e6134426d7e35e1ff9356dfb97
Alexis Wurth <alexis.wurth57@gmail.com>
2017-02-20 11:56:44 +0100

    Added support for the CORS Preflight request

--- bootstrap/routes.php
+++ bootstrap/routes.php
@@ -2,6 +2,10 @@
 
 $dir = __DIR__ . '/../src/App/Resources/routes/';
 
+$app->options('/{routes:.+}', function ($request, $response) {
+    return $response;
+});
+
 require $dir . 'auth.php';
 require $dir . 'category.php';
 require $dir . 'product.php';
